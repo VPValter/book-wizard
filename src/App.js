c538748c7da2a2794b5a0e70593b390b35cffdfc
Vladimir Perić <mgr.art.v.peric@gmail.com>
2022-04-16 14:39:46 +0200

    fetching genres

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const getGenres = () => {
+    fetch('genres.json', {
+      headers:  {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }).then(res => {
+      return res.json();
+    }).then(genres => {
+      setGenres(genres);
+    });
+  }
+
+  useEffect(() => {
+    getGenres();
+  }, []);
+
+  const [genres, setGenres] = useState({});
+
   return (
     <div className="App">
       App
