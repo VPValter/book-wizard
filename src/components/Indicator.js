05936b07a83bd689a12ad201f5b11c4e2c9cc00b
Vladimir Perić <mgr.art.v.peric@gmail.com>
2022-04-17 12:07:24 +0200

    indicator component

--- src/components/Indicator.js
+++ src/components/Indicator.js
@@ -1,5 +1,59 @@
+import { useLocation } from 'react-router-dom';
+
 const Indicator = () => {
-    return (<h2>Indicator component</h2>);
-}
+  const location = useLocation();
+
+  return (
+    <div className='indicator'>
+      <div className='indicator-item flex-column'>
+        <span
+          className={
+            location.pathname.includes('/step1') ? 'number selected' : 'number'
+          }
+        >
+          1
+        </span>
+        <span className='label'>Genre</span>
+      </div>
+      <div className='indicator-item flex-column'>
+        <span
+          className={
+            location.pathname.includes('/step2') ? 'number selected' : 'number'
+          }
+        >
+          2
+        </span>
+        <span className='label'>Subgenre</span>
+      </div>
+
+      {location.pathname.includes('/step') && (
+        <div className='indicator-item flex-column'>
+          <span className='number'>...</span>
+        </div>
+      )}
+
+      {location.pathname.includes('/add-new') && (
+        <div className='indicator-item flex-column'>
+          <span className='number selected'>3</span>
+          <span className='label'>Add new subgenre</span>
+        </div>
+      )}
+
+      {location.pathname.includes('/final') && (
+        <>
+          <div className='indicator-item flex-column'>
+            <span className='number'>3</span>
+            <span className='label'>Add new subgenre</span>
+          </div>
+          <div className='indicator-item flex-column'>
+            <span className='number selected'>4</span>
+            <span className='label'>Information</span>
+          </div>
+        </>
+      )}
+
+    </div>
+  );
+};
 
 export default Indicator;
